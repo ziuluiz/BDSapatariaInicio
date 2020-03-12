e7e3d56aaccaf754638cd8116b1e01ae4d576144
victor hugo <victorhugo_sp4@hotmail.com>
2020-03-12 19:44:04 -0300

    tela de login

--- BDSapataria/View/TelaDeLogin.cs
+++ BDSapataria/View/TelaDeLogin.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BDSapataria
+namespace BDSapataria.View
 {
-    public partial class Form1 : Form
+    public partial class TelaDeLogin : Form
     {
-        public Form1()
+        public TelaDeLogin()
         {
             InitializeComponent();
         }
