0ceab5d5b55b12e73583777a5e633b28ad0df531
Luiz Henrique_ <luiz_henrique_silva@outlook.com>
2020-03-12 21:20:07 -0300

    manipulaCliente

--- BDSapataria/Program.cs
+++ BDSapataria/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BDSapataria.View;
 
 namespace BDSapataria
 {
@@ -16,7 +17,7 @@ namespace BDSapataria
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new TelaDeLogin());
         }
     }
 }
